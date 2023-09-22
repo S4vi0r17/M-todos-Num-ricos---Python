652e75199c86ce8e5f7601aa1c7b2c39dbd062a5
S4vi0r <gbp17@outlook.com>
2023-09-22 00:20:52 -0500

    Minor commit

--- Semana3/Eliminacion de Gaus/forward.py
+++ Semana3/Eliminacion de Gaus/forward.py
@@ -1,8 +1,14 @@
 import numpy as np
 
-A = np.array([[1.0, 0.0, 0.0], [0.5, 1.0, 0.0], [2.0, 2.0, 1.0]])
+A = np.array([[1.0, 1.0, 1.0], [0.0, 2.0, -1.0], [3.0, -2.0, 1.0]])
 
-b = np.array([2.0, 2.0, 4.0])
+b = np.array([6.0, 3.0, 4.0])
+
+AB = np.column_stack((A, b))
+
+filas, columnas = AB.shape
+
+# Eliminación gaussiana (inferior)
 
 def sustitucion_hacia_adelante(matriz, vector):
     n = len(vector)
@@ -14,6 +20,6 @@ def sustitucion_hacia_adelante(matriz, vector):
 
     return x
 
-solucion = sustitucion_hacia_adelante(A, b)
+solucion = sustitucion_hacia_adelante(A_escalonada, b_escalonado)  # Utiliza A_escalonada y b_escalonado aquí
 
 print("Solución:", solucion)
